7897bf17814eb56120e65b10557ad0eef99a1372
Codewithjay7 <jay1971chavda@gmail.com>
2025-07-11 11:00:52 +0530

    My-Portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,18 @@
+export const words = [
+  {
+    text: "Tdeas",
+    imgpath: "/images/ideas.svg",
+  },
+  {
+    text: "Concepts",
+    imgpath: "/images/concepts.svg",
+  },
+  {
+    text: "Designs",
+    imgpath: "/images/designs.svg",
+  },
+  {
+    text: "Code",
+    imgpath: "/images/code.svg",
+  },
+];
